498ac097d573ebd150fe7461acb01edf1a87d6e7
Brian <brian.merlob@gmail.com>
2025-05-03 19:07:35 -0700

    Try without base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   assetPrefix: "./",
-  basePath: "/corgi-sql",
   images: {
     unoptimized: true,
   },
