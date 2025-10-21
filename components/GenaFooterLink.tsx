7151991e9de99d574a65e52690ffd6d61c3c73e3
tuan-h <tuan.a.huynh@gmail.com>
2025-10-21 10:59:37 +0900

    remove logos

--- components/GenaFooterLink.tsx
+++ components/GenaFooterLink.tsx
@@ -1,34 +1,9 @@
 "use client";
 
-import { Link } from "@heroui/react";
 import React from "react";
 
-import Image from "next/image";
-
-import genalLogo from "@/assets/gena_logo.png";
-import cornellLogo from "@/assets/cornell_logo.png";
-
 function GenaFooterLink() {
-  return (
-    <div className="flex gap-8 text-nowrap items-center">
-      <div className="flex-1">
-        <Image
-          alt="Gena logo"
-          src={genalLogo}
-          width={40 * 574/140}
-          height={40}
-        />
-      </div>
-      <div className="flex-1">
-        <Image
-          alt="Cornell logo"
-          src={cornellLogo}
-          width={50 * 2252/572}
-          height={50}
-        />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default GenaFooterLink;
--- components/GenaNavbar.tsx
+++ components/GenaNavbar.tsx
@@ -6,21 +6,15 @@ import {
   NavbarContent,
 } from "@heroui/react";
 import NextLink from "next/link";
-import Image from "next/image";
 
 import NavbarLink from "@/components/NavbarLink";
-import whiteCornellLogo from "@/assets/cornell_seal_simple_web_white.svg";
-import genaWhiteLogo from "@/assets/gena_green_white.svg";
 
 function Navbar() {
   return (
     <NextUINavbar className="bg-carnelian" maxWidth="xl">
       <NavbarBrand className="hidden sm:block">
         <NextLink href="/">
-          <div className="flex gap-8 text-nowrap">
-            <Image alt="Gena logo" src={genaWhiteLogo} width="75" />
-            <Image alt="Cornell logo" src={whiteCornellLogo} width="40" />
-          </div>
+          <div></div>
         </NextLink>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex text-white" justify="end">
